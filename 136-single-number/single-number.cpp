81ef31954427a6e50121702c66013c6066ce08f2
vsowji15 <vemulapudisowjanya1515@gmail.com>
2026-03-25 11:28:39 +0530

    Time: 13 ms (10.85%) | Memory: 23.9 MB (7.65%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int>mp;
+        for(int i:nums){
+            mp[i]++;
+        }
+        for(auto it:mp){
+            if(it.second==1){
+                return it.first;
+            }
+        }
+        return 0;
+    }
+};
