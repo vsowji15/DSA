07659cd92d55da5fed28e3f712ba6d23508eab8c
vsowji15 <vemulapudisowjanya1515@gmail.com>
2026-05-27 18:42:22 +0530

    Time: 60 ms (93.26%) | Memory: 24.2 MB (80.34%) - LeetSync

--- 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.cpp
+++ 3405-count-the-number-of-special-characters-ii/count-the-number-of-special-characters-ii.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
-        vector<int>sml(26,0);
-        vector<int>lrg(26,0);
-        vector<int>fst(26,0);
+        vector<int>sml(26,-1);
+        vector<int>lrg(26,-1);
         int idx;
         for(int i=0;i<word.size();i++){
             if(word[i]<='z' && word[i]>='a'){
                 idx=word[i]-'a';
-                sml[idx]++;
+                sml[idx]=i;
             }
             else{
                 idx=word[i]-'A';
-                lrg[idx]++;
-                if(lrg[idx]==1){
-                    fst[idx]=sml[idx];
+                if(lrg[idx]==-1){
+                   lrg[idx]=i;
                 }
             }
         }
         int cnt=0;
         for(int i=0;i<26;i++){
-            if(sml[i]>0 && lrg[i]>0 && sml[i]==fst[i]){
+            if(sml[i]>-1 && lrg[i]>-1 && sml[i]<lrg[i]){
                 cnt++;
             }
         }
