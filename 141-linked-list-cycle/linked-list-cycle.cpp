e227a999135e9dd7cd7a4e5e87599d519a198e5e
vsowji15 <vemulapudisowjanya1515@gmail.com>
2026-09-06 11:55:49 +0530

    Time: 12 ms (41.10%) | Memory: 11.6 MB (97.28%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -9,15 +9,14 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        ListNode* slow=head;
-        ListNode* fast=head;
-        while(fast!=nullptr && fast->next!=nullptr){
-            slow=slow->next;
-            fast=fast->next->next;
-            if(slow==fast){
-                return true;
-            }
+       ListNode* slow=head,*fast=head;
+       while(fast && fast->next){
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast){
+            return true;
         }
-        return false;
+       }
+       return false;
     }
 };
